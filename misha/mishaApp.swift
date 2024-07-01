6bf82679aa97f3c1f20593a25efe5f3415e83f0d
IFraimG <kulsasha55@gmail.com>
2024-07-01 17:29:36 +0300

    Initial Commit

---
+++ misha/mishaApp.swift
@@ -0,0 +1,17 @@
+//
+//  mishaApp.swift
+//  misha
+//
+//  Created by MacBook on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct mishaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
